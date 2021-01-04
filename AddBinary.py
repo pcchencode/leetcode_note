79c76d6e07d747f703e9521082f32561aae7f61e
pcchencode <zxp791228@gmail.com>
2021-01-04 21:38:48 +0800

    upload solution

--- AddBinary.py
+++ AddBinary.py
@@ -1,14 +1,25 @@
-a = "11"
-b = "1"
+# 67. Add Binary
+# Given two binary strings a and b, return their sum as a binary string.
 
-dec_a = int(a, 2)
-dec_b = int(b, 2)
+# Example 1:
 
-res = str(dec_a + dec_b)
+# Input: a = "11", b = "1"
+# Output: "100"
+# Example 2:
 
-bin_res = bin(int(res))
+# Input: a = "1010", b = "1011"
+# Output: "10101"
 
-print(bin_res)
-print(bin_res[2:])
+import os
 
-#print(int("11", 2))
+def AddBinary(a, b):
+    dec_a = int(a, 2)
+    dec_b = int(b, 2)
+
+    res = str(dec_a + dec_b)
+
+    bin_res = bin(int(res))
+    return bin_res[2:]
+
+print(AddBinary("1010", "1011"))
+os._exit(0)
