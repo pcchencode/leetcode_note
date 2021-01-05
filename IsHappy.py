c610bcf6cc983fa2ef2f4149e6f4c7e2f8fae907
pcchencode <zxp791228@gmail.com>
2021-01-06 07:47:55 +0800

    add some note

--- IsHappy.py
+++ IsHappy.py
@@ -15,21 +15,20 @@ def isHappy(n):
     while True:
         tmp = split_sum(n)
         if tmp == 1:
+            print(tmp)
             print("happy!!")
             return True
             break
         else:
             split_result.append(tmp)
             n = split_sum(n)
-            n2 = split_sum(n)
-            if n2 in split_result:
+            next2_split = split_sum(n)
+            if next2_split in split_result:
                 print("not happy")
                 return False
                 break
-            # if n == init:
-            #     print("not happy")
-            #     break
             print(tmp)
             pass
 
 print(isHappy(2))
+print(isHappy(19))
