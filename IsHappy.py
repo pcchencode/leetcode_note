1f86d7e134ce23f3393a085cbb22c6af44b52acf
pcchencode <zxp791228@gmail.com>
2021-01-06 00:09:25 +0800

    upload happy solution

--- IsHappy.py
+++ IsHappy.py
@@ -6,20 +6,30 @@ def split_sum(num):
     for i in s:
        res += int(i)*int(i)
     # print(res)
-
     return res
 
-# split_sum(19)
-n = 2
-while True:
-    tmp = split_sum(n)
-    if tmp == 1:
-        print("happy!!")
-        break
-    else:
-        n = split_sum(n)
-        if n == tmp:
-            print("not happy")
+def isHappy(n):
+    # split_sum(19)
+    # n = 2
+    split_result = []
+    while True:
+        tmp = split_sum(n)
+        if tmp == 1:
+            print("happy!!")
+            return True
             break
-        print(tmp)
-        pass
+        else:
+            split_result.append(tmp)
+            n = split_sum(n)
+            n2 = split_sum(n)
+            if n2 in split_result:
+                print("not happy")
+                return False
+                break
+            # if n == init:
+            #     print("not happy")
+            #     break
+            print(tmp)
+            pass
+
+print(isHappy(2))
