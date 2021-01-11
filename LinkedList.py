b6b273f363ae5e659264c3f17ef558e5e630ea97
pcchencode <zxp791228@gmail.com>
2021-01-11 18:31:09 +0800

    add node ex

--- LinkedList.py
+++ LinkedList.py
@@ -4,3 +4,14 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+h = ListNode(3)
+h.next = ListNode(2)
+h.next.next = ListNode(0)
+h.next.next.next = ListNode(4)
+h.next.next.next.next = h.next
+
+print(h.next.val)
+
+
+os._exit(0)
