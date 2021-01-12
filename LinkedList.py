4bf2b09ab6ce0dc427b524efad8fc1206fba3e90
pcchencode <zxp791228@gmail.com>
2021-01-12 11:59:07 +0800

    update solution

--- LinkedList.py
+++ LinkedList.py
@@ -5,35 +5,48 @@ class ListNode:
         self.val = x
         self.next = None
 
-# h = ListNode(3)
-# h.next = ListNode(2)
-# h.next.next = ListNode(0)
-# h.next.next.next = ListNode(4)
-# h.next.next.next.next = h.next
-# print(id(h.next))
-# print(id(h.next.next.next.next))
+
+#n1 = ListNode(3)
+#n1.next = ListNode(4)
+#n2 = ListNode(3)
+#n2.next = ListNode(4)
+#print(id(n1))
+#print(id(n2))
+#print(n1.next.next)
+#os._exit(0)
+
 
 h = ListNode(3)
 h.next = ListNode(2)
 h.next.next = ListNode(0)
 h.next.next.next = ListNode(4)
-h.next.next.next.next = ListNode(2)
+h.next.next.next.next = h.next
+# print(id(h.next))
+# print(id(h.next.next.next.next))
+
+# h = ListNode(3)
+# h.next = ListNode(2)
+# h.next.next = ListNode(0)
+# h.next.next.next = ListNode(4)
+# h.next.next.next.next = ListNode(2)
 # print(id(h.next))
 # print(id(h.next.next.next.next))
 
 # print(h.next.val)
 
 addr_lst = []
-addr_lst.append(id(h.val))
-while h.next != None:
-	# print(h)
-	if id(h.next.val) not in addr_lst:
-		addr_lst.append(id(h.next.val))
-	else:
-		print("cycle!!")
-		break
-	if h.next != None:
-		h = h.next
+addr_lst.append(id(h))
+if h:
+    while h.next != None:
+        # print(h)
+        if id(h.next) not in addr_lst:
+            print(id(h.next))
+            addr_lst.append(id(h.next))
+        else:
+            print("cycle!!")
+            break
+        if h.next != None:
+           h = h.next
 
 print(addr_lst)
 
