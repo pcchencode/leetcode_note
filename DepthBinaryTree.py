64a67b33edc2c91453df43ff5e59f8c7ee3d21e1
pcchencode <zxp791228@gmail.com>
2021-01-16 00:25:55 +0800

    max depth

--- DepthBinaryTree.py
+++ DepthBinaryTree.py
@@ -8,11 +8,51 @@ class TreeNode:
         self.left = left
         self.right = right
 
-# 使用dfs搜尋，只要沒碰到leaf就+1; 只要碰到leaf就歸0
-c1l = TreeNode(3)
-c1r = TreeNode(4)
-c2l = TreeNode(4)
-c2r = TreeNode(3)
-c1 = TreeNode(val=2, left=c1l, right=c1r)
-c2 = TreeNode(val=2, left=c2l, right=c2r)
+# 使用dfs搜尋，只要沒碰到leaf就+1; 只要碰到leaf就扣1，並且將當時走道的depth輸入一個list
+c1l = None
+c1r = None
+c2l = TreeNode(15)
+c2r = TreeNode(7)
+c1 = TreeNode(val=9, left=c1l, right=c1r)
+c2 = TreeNode(val=20, left=c2l, right=c2r)
 r = TreeNode(val=1, left=c1, right=c2)
+
+def isleaf(r):
+    if r.left:
+        return False
+    elif r.right:
+        return False
+    else:
+        return True
+
+
+def dfs_left(root):
+    result = []
+    stack = []
+    depth = 1
+    stack.append(root)
+    while len(stack)>0:
+        node = stack.pop()
+        if not isleaf(node):
+            depth += 1
+        
+        if type(node) is TreeNode:
+            result.append(node.val)
+
+            if node.right != None:
+                stack.append(node.right)
+
+            if node.left != None:
+                stack.append(node.left)
+
+        else:
+            result.append(node)
+
+
+    print(result)
+    print(depth)
+    return result
+
+dfs_left(r)
+
+
