b0458660a410b001344dbb408511d11123a48337
pcchencode <zxp791228@gmail.com>
2021-01-15 23:13:17 +0800

    update code

--- DepthBinaryTree.py
+++ DepthBinaryTree.py
@@ -9,3 +9,10 @@ class TreeNode:
         self.right = right
 
 # 使用dfs搜尋，只要沒碰到leaf就+1; 只要碰到leaf就歸0
+c1l = TreeNode(3)
+c1r = TreeNode(4)
+c2l = TreeNode(4)
+c2r = TreeNode(3)
+c1 = TreeNode(val=2, left=c1l, right=c1r)
+c2 = TreeNode(val=2, left=c2l, right=c2r)
+r = TreeNode(val=1, left=c1, right=c2)
