f0a777958e98acec250782244d078ac7516f23a6
pcchencode <zxp791228@gmail.com>
2023-05-06 17:41:43 +0800

    update class MyLinkedList

--- LinkedList.py
+++ LinkedList.py
@@ -1,5 +1,56 @@
 import os
 
+class MyLinkedList:
+
+    def __init__(self):
+        self.arr = [] #感覺本身就是要存一個資料結構
+
+    def get(self, index: int) -> int:
+        if index > len(self.arr)-1:
+            pass
+        else:
+            return self.arr[index]
+
+    def addAtHead(self, val: int) -> None:
+        self.arr.insert(0, val)
+        pass
+
+    def addAtTail(self, val: int) -> None:
+        self.arr.append(val)
+        pass
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        self.arr.insert(index, val)
+        pass
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index > len(self.arr)-1:
+            pass
+        else:
+            self.arr.pop(index)
+        pass
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+obj = MyLinkedList()
+# param_1 = obj.get(index)
+print(obj.get(1))
+obj.addAtHead(4)
+print(obj.get(1))
+obj.addAtHead(1)
+obj.addAtHead(5)
+obj.deleteAtIndex(3)
+obj.addAtHead(7)
+print(obj.get(3))
+print(obj.get(3))
+print(obj.get(3))
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
+
+os._exit(0)
+
+
 class ListNode:
     def __init__(self, x):
         self.val = x
