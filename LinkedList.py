fefc0fc15969da1f987f96c82e2b4f0d8ab5b739
pcchencode <zxp791228@gmail.com>
2023-05-06 22:59:06 +0800

    udpate linked list operation

--- LinkedList.py
+++ LinkedList.py
@@ -1,4 +1,118 @@
 import os
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def get_length(self):
+        res = 0; curr = self.head
+        while curr:
+            res += 1
+            curr = curr.next
+        return res
+            
+    def get(self, index: int) -> int:
+        if not self.head:
+            return -1
+        length = self.get_length()
+        # print("length:", length)
+        if index > length -1:
+            return -1 
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        if self.head:
+            result = ListNode(val)
+            result.next = self.head
+            self.head = result
+        else:
+            self.head = ListNode(val)
+        pass
+
+    def addAtTail(self, val: int) -> None:
+        result = ListNode(0)
+        curr = result
+        while self.head:
+            curr.next = ListNode(self.head.val)
+
+            curr = curr.next
+            self.head = self.head.next
+        if self.head is None:
+            curr.next = ListNode(val)
+        self.head = result.next
+        pass
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        length = self.get_length()
+        if index > length:
+            return
+        
+        result = ListNode(0)
+        curr = result
+        for i in range(index):
+            curr.next = ListNode(self.head.val)
+            curr = curr.next
+            self.head = self.head.next
+        remain_node = self.head
+        curr.next = ListNode(val)
+        curr.next.next = remain_node
+
+        self.head = result.next
+        pass
+
+    def deleteAtIndex(self, index: int) -> None:
+        length = self.get_length()
+        if index > length-1:
+            return
+        result = ListNode(0)
+        curr = result
+        for i in range(index):
+            curr.next = ListNode(self.head.val)
+            curr = curr.next
+            self.head = self.head.next
+        remain_node = self.head.next
+        curr.next = remain_node
+
+        self.head = result.next
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+obj = MyLinkedList()
+obj.addAtHead(4)
+obj.addAtHead(1)
+obj.addAtHead(5)
+obj.addAtTail(9)
+obj.addAtTail(6)
+obj.addAtTail(3)
+obj.addAtTail(2)
+obj.addAtIndex(2, 87)
+obj.deleteAtIndex(5)
+# a = obj.get(2)
+# print(a)
+print("===")
+for i in range(8):
+    print(obj.get(i))
+
+
+# print(obj.head.next.next.val)
+# print(obj.get(1))
+# print(obj.get(2))
+# print(obj.get(3))
+
+# print(obj.length)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
+
+os._exit(0)
+
 
 class MyLinkedList:
 
