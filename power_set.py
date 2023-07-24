e8b010507a4967c9c65511849ccf74c5b5b43b20
pcchencode <zxp791228@gmail.com>
2023-07-24 15:46:35 +0800

    add backtrack solution

--- power_set.py
+++ power_set.py
@@ -18,5 +18,28 @@ nums = [1,2,3,4]
 res = [[]]
 for t in nums:
     res = helper(res, t)
-
 print(res)
+
+##回溯正解
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.result = []
+        self.path = [[]]
+
+        def backtrack(nums):
+            #終止條件
+            if len(nums)==0:
+                return
+
+            for i in range(0, len(nums)):
+                cand = nums[i]; remains = nums[i+1:]
+                res = self.path[-1].copy(); res.append(cand)
+                self.path.append(res)
+                backtrack(remains)
+
+                self.result.append(self.path[-1].copy()) #回溯時放入
+                self.path = self.path[:-1]
+            
+        backtrack(nums)
+        self.result.append([])
+        return self.result
